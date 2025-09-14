09ec55086d4b3ea84002a2083c406d0f3e18c0b5
Vnz-007 <viniciusloiolajg@gmail.com>
2025-09-13 22:44:44 -0300

    Cards Complete

--- app/app.vue
+++ app/app.vue
@@ -1,46 +1,65 @@
 <template>
-  <div class="text-white">
-    <h1 class="blue-500">Rick and Morty</h1>
+  <main class="text-white bg-[#1e1e1e]">
+    <section class="flex flex-col w-full max-w-[1224px] mx-auto gap-8">
+      <header class="flex gap-4">
+        <h2 class="text-2xl font-bold">Personagens</h2>
 
-    <div class="flex flex-wrap w-full max-w-[1224px] mx-auto gap-4">
-      <div
-        v-for="currentCharacter of data.results"
-        class="flex flex-col gap-4 p-4 rounded-lg bg-[#313234]"
-      >
-        <img
-          :src="currentCharacter.image"
-          height="200"
-          width="262"
-          class="rounded-2xl"
-        />
+        <button class="flex gap-2 p-1 pr-2 rounded-[32px] bg-[#313234]">
+          <IconsSquareFour />
+          Ver todos
+        </button>
+      </header>
 
-        <div class="grid grid-cols-[1fr,48px]">
-          <div class="flex flex-col gap-4">
-            <p class="text-base font-bold">{{ currentCharacter.name }}</p>
+      <div class="flex flex-wrap gap-4">
+        <div
+          v-for="currentCharacter of data.results"
+          class="w-full max-w-[294px] flex flex-col gap-4 p-4 rounded-lg bg-[#313234]"
+        >
+          <img
+            :src="currentCharacter.image"
+            height="200"
+            width="262"
+            class="rounded-2xl h-[200px] object-cover"
+          />
 
-            <div class="flex flex-col gap-2">
-              <p>
-                {{ currentCharacter.status === "Alive" ? "Vivo" : "Morto" }}
-              </p>
+          <div class="grid grid-cols-[1fr,48px]">
+            <div class="flex flex-col gap-4">
+              <p class="text-base font-bold">{{ currentCharacter.name }}</p>
 
-              <p>{{ currentCharacter.species }}</p>
-              <p>{{ currentCharacter.origin.name }}</p>
+              <div class="flex flex-col gap-2">
+                <p>
+                  {{ currentCharacter.status === "Alive" ? "Vivo" : "Morto" }}
+                </p>
+
+                <p>{{ currentCharacter.species }}</p>
+                <p>{{ currentCharacter.origin.name }}</p>
+              </div>
             </div>
+
+            <span>
+              <IconsHeartFilled v-if="currentCharacter.status === 'Alive'" />
+              <IconsHeartOutlined v-else />
+            </span>
           </div>
 
-          <span>
-            <IconsHeartFilled />
-            <IconsHeartOutlined />
-          </span>
+          <NuxtLink
+            :to="currentCharacter.url"
+            class="flex items-center self-end py-[6px] pl-1 pr-2 mt-auto gap-2 text-sm rounded-[32px] bg-[#11b0c0]"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <IconsInfo />
+            Saiba Mais
+          </NuxtLink>
         </div>
-
-        <NuxtLink :to="currentCharacter.url" class="mt-2">Saiba Mais</NuxtLink>
       </div>
-    </div>
-  </div>
+    </section>
+  </main>
 </template>
 
 <script lang="ts" setup>
+import IconsInfo from "../components/icons/Info.vue";
+import IconsSquareFour from "../components/icons/SquareFour.vue";
 import IconsHeartFilled from "../components/icons/HeartFilled.vue";
 import IconsHeartOutlined from "../components/icons/HeartOutlined.vue";
 
