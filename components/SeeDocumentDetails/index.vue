d869a98d5066a6fd5da4c96108d57f7a09770fa7
Vnz-007 <viniciusloiolajg@gmail.com>
2025-09-14 19:23:30 -0300

    Index.vue & justify-between in desc cards

---
+++ components/SeeDocumentDetails/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <NuxtLink
+    :to="actionUrl"
+    class="flex items-center self-end py-[6px] pl-1 pr-2 mt-auto gap-2 text-sm rounded-[32px] bg-[#11B0C8]"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    <IconsInfo />
+    Saiba Mais
+  </NuxtLink>
+</template>
+
+<script setup>
+import IconsInfo from "../components/icons/Info.vue";
+
+const props = defineProps({
+  actionUrl: String,
+});
+</script>
