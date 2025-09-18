6738837f4c7d390656569b123935140e2660bd3c
Vnz-007 <viniciusloiolajg@gmail.com>
2025-09-18 17:46:59 -0300

    HeaderLogo cell

--- components/HeaderLogo/index.vue
+++ components/HeaderLogo/index.vue
@@ -2,7 +2,7 @@
   <header>
     <PageContainer>
       <div
-        class="absolute w-full max-w-[1220px] top-10 flex items-center justify-between"
+        class="absolute top-10 w-full max-w-[1220px] flex flex-col md:flex-row items-start md:items-center justify-between gap-4"
       >
         <img
           src="/images/brands/Logo-Header.png"
