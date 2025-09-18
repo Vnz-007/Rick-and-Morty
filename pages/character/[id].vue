03c8bbbc4462e0d55f1c92aa791ba20273c2ef16
Vnz-007 <viniciusloiolajg@gmail.com>
2025-09-18 19:20:02 -0300

    details

--- pages/character/[id].vue
+++ pages/character/[id].vue
@@ -2,44 +2,40 @@
   <section>
     <HeaderLogo class="bg-transparent" />
     <PageContainer class="pt-8">
-      <div>
-        <div
-          class="flex flex-col items-center justify-center md:flex-row md:gap-16 gap-8"
-        >
-          <img
-            :src="data.image"
-            alt=""
-            width="369"
-            height="461"
-            class="w-[300px] rounded-2xl"
-          />
+      <div class="flex flex-col md:flex-row md:gap-16 gap-8">
+        <img
+          :src="data.image"
+          alt=""
+          width="369"
+          height="461"
+          class="w-[300px] rounded-2xl"
+        />
 
-          <div class="flex flex-col md:gap-12 gap-8">
-            <div class="flex flex-wrap items-center gap-4">
-              <h1 class="text-4xl md:text-5xl leading-[60px]">
-                {{ data.name }}
-              </h1>
-              <IconsHeartOutlined width="40" />
+        <div class="flex flex-col md:gap-12 gap-8">
+          <div class="flex flex-wrap items-center gap-4">
+            <h1 class="text-4xl md:text-5xl leading-[60px]">
+              {{ data.name }}
+            </h1>
+            <IconsHeartOutlined width="40" />
+          </div>
+
+          <div class="flex items-center gap-2 text-2xl">
+            <IconsPlay />
+            <p>Participou de {{ data.episode.length }} episódios</p>
+          </div>
+
+          <div class="flex gap-6 text-2xl">
+            <div class="flex items-center justify-center">
+              <IconsPulse width="32" />
+              <span>{{ data.status === "Alive" ? "Vivo" : "Morto" }}</span>
             </div>
-
-            <div class="flex items-center gap-2 text-2xl">
-              <IconsPlay />
-              <p>Participou de {{ data.episode.length }} episódios</p>
+            <div class="flex items-center justify-center">
+              <IconsEspecie width="32" />
+              <span>{{ data.species }}</span>
             </div>
-
-            <div class="flex gap-6 text-2xl">
-              <div class="flex items-center justify-center">
-                <IconsPulse width="32" />
-                <span>{{ data.status === "Alive" ? "Vivo" : "Morto" }}</span>
-              </div>
-              <div class="flex items-center justify-center">
-                <IconsEspecie width="32" />
-                <span>{{ data.species }}</span>
-              </div>
-              <div class="flex items-center justify-center">
-                <IconsGender width="32" />
-                <span>{{ data.gender }}</span>
-              </div>
+            <div class="flex items-center justify-center">
+              <IconsGender width="32" />
+              <span>{{ data.gender }}</span>
             </div>
           </div>
         </div>
