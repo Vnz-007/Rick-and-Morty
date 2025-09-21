6b122c207ca2d60f22238c5f5bcae16aa3ff5419
Vnz-007 <viniciusloiolajg@gmail.com>
2025-09-21 18:06:07 -0300

    Favorite Characters Ok

--- pages/favorite.vue
+++ pages/favorite.vue
@@ -1,28 +1,4 @@
 <template>
-  <section class="flex flex-wrap gap-4">
-    <div
-      v-for="card in favoritos"
-      :key="card.id"
-      class="p-4 rounded-lg shadow bg-white"
-    >
-      <h3>{{ card.name }}</h3>
-      <button
-        @click="toggleLike(card.id)"
-        class="mt-2 px-3 py-1 bg-red-500 text-white rounded"
-      >
-        Remover dos favoritos
-      </button>
-    </div>
-
-    <p v-if="!pending && favoritos.length === 0" class="text-gray-500">
-      Nenhum favorito ainda.
-    </p>
-
-    <p v-if="pending" class="text-gray-500">Carregando favoritos...</p>
-  </section>
-</template>
-
-<!-- <template>
   <header class="border-b-2 border-cyan-500">
     <HeaderLogoC class="bg-transparent" />
     <PageContainer>
@@ -45,21 +21,85 @@
       </div>
     </PageContainer>
   </header>
+
   <PageContainer class="pt-16">
-    <CharactersListing />
-    <EpisodesListing />
-    <LocationListing />
+    <section class="flex flex-col pb-20 mx-auto">
+      <div class="flex flex-wrap gap-7 justify-center xl:justify-start">
+        <div class="flex gap-4 items-center">
+          <h2 class="text-2xl font-bold">Personagens</h2>
+          <button
+            @click="mostrarTodos()"
+            class="flex gap-2 p-1 pr-2 rounded-[32px] bg-[#313234]"
+          >
+            <IconsSquareFour />
+            {{ showAll ? "Ver menos" : "Ver todos" }}
+          </button>
+        </div>
+
+        <div class="flex flex-wrap gap-4 justify-center">
+          <Card
+            v-for="(card, index) in favoritos"
+            v-show="showAll || index < 8"
+            :key="card.id"
+            class="w-full max-w-[294px] flex flex-col gap-4 p-4 rounded-lg"
+          >
+            <img
+              :src="card.image"
+              height="200"
+              width="262"
+              class="rounded-2xl h-[200px] object-cover"
+            />
+            <div class="grid grid-cols-[1fr,48px]">
+              <div class="flex flex-col gap-4">
+                <p class="text-base font-bold">{{ card.name }}</p>
+
+                <div class="flex flex-col gap-2">
+                  <div class="flex items-center gap-2">
+                    <IconsPulse />
+                    <p>
+                      {{ card.status === "Alive" ? "Vivo" : "Morto" }}
+                    </p>
+                  </div>
+                  <div class="flex items-center gap-2">
+                    <IconsEspecie />
+                    <p>{{ card.species }}</p>
+                  </div>
+                  <div class="flex items-center gap-2">
+                    <IconsPlanet />
+                    <p>{{ card.origin.name }}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <button @click="toggleLike(card.id)" class="">
+              <IconsHeartFilled />
+            </button>
+          </Card>
+        </div>
+      </div>
+
+      <p v-if="!pending && favoritos.length === 0" class="text-gray-500">
+        Nenhum favorito ainda.
+      </p>
+
+      <p v-if="pending" class="text-gray-500">Carregando favoritos...</p>
+    </section>
   </PageContainer>
-</template> -->
+</template>
 
 <script setup>
 import { computed } from "vue";
 
 const { characters, fetchCharacters, toggleLike } = useCharacters();
 const pending = ref(true);
+const favoritos = computed(() => characters.value.filter((c) => c.liked));
+const showAll = ref(false);
 
 await fetchCharacters();
 pending.value = false;
 
-const favoritos = computed(() => characters.value.filter((c) => c.liked));
+function mostrarTodos() {
+  showAll.value = !showAll.value;
+}
 </script>
