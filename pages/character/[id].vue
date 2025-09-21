c344b6ec31b461444647d772e7325857375f39ed
Vnz-007 <viniciusloiolajg@gmail.com>
2025-09-21 09:35:20 -0300

    Add More Characters in Page

--- pages/character/[id].vue
+++ pages/character/[id].vue
@@ -1,47 +1,52 @@
 <template>
   <section>
     <HeaderLogoB class="bg-transparent" />
-    <PageContainer class="pt-8">
-      <div class="flex flex-col items-center md:flex-row md:gap-16 gap-8">
-        <img
-          :src="data.image"
-          alt=""
-          width="369"
-          height="461"
-          class="w-[300px] rounded-2xl"
-        />
+    <div class="pb-16 border-b-2 border-cyan-500">
+      <PageContainer class="pt-8">
+        <div class="flex flex-col items-center md:flex-row md:gap-16 gap-8">
+          <img
+            :src="data.image"
+            alt=""
+            width="369"
+            height="461"
+            class="w-[300px] rounded-2xl"
+          />
 
-        <div class="flex flex-col md:gap-12 gap-8">
-          <div class="flex flex-wrap items-center gap-4">
-            <h1 class="text-4xl md:text-5xl font-bold leading-[60px]">
-              {{ data.name }}
-            </h1>
-            <IconsHeartOutlined :width="56" :height="56" />
-          </div>
-
-          <div class="flex items-center gap-2 text-2xl">
-            <IconsPlay />
-            <p>Participou de {{ data.episode.length }} episódios</p>
-          </div>
-
-          <div class="flex items-center justify-between text-lg md:text-2xl">
-            <div class="flex items-center justify-center gap-2">
-              <IconsPulse :width="32" :height="32" />
-              <span>{{ data.status === "Alive" ? "Vivo" : "Morto" }}</span>
+          <div class="flex flex-col md:gap-12 gap-8">
+            <div class="flex flex-wrap items-center gap-4">
+              <h1 class="text-4xl md:text-5xl font-bold leading-[60px]">
+                {{ data.name }}
+              </h1>
+              <IconsHeartOutlined :width="56" :height="56" />
             </div>
 
-            <div class="flex items-center justify-center gap-2">
-              <IconsEspecie :width="32" :height="32" />
-              <span>{{ data.species }}</span>
+            <div class="flex items-center gap-2 text-2xl">
+              <IconsPlay />
+              <p>Participou de {{ data.episode.length }} episódios</p>
             </div>
 
-            <div class="flex items-center justify-center gap-2">
-              <IconsGender :width="32" :height="32" />
-              <span>{{ data.gender }}</span>
+            <div class="flex items-center justify-between text-lg md:text-2xl">
+              <div class="flex items-center justify-center gap-2">
+                <IconsPulse :width="32" :height="32" />
+                <span>{{ data.status === "Alive" ? "Vivo" : "Morto" }}</span>
+              </div>
+
+              <div class="flex items-center justify-center gap-2">
+                <IconsEspecie :width="32" :height="32" />
+                <span>{{ data.species }}</span>
+              </div>
+
+              <div class="flex items-center justify-center gap-2">
+                <IconsGender :width="32" :height="32" />
+                <span>{{ data.gender }}</span>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </PageContainer>
+    </div>
+    <PageContainer class="text-white">
+      <CharactersListing class="" />
     </PageContainer>
   </section>
 </template>
